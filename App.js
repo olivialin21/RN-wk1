3cc588a1116f28f512d58906888a9c62347eb808
olivialin21 <olivia891031@gmail.com>
2022-02-23 00:11:22 +0800

    change view

--- App.js
+++ App.js
@@ -4,7 +4,8 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={styles.num}>110819023</Text>
+      <Text style={styles.font}>林渝珊</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +14,17 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#ff8a75',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  num:{
+    fontSize: 24,
+    color: '#ffffff',
+  },
+  font: {
+    fontSize: 36,
+    color: '#ffffff',
+    fontWeight: 'bold',
+  }
 });
